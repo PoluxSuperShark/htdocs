bed016cf79eb08e4e6556b44cde8716468646fc4
PoluxSuperShark <contact@poluxsupershark.net>
2026-02-23 21:21:16 +0100

    Added few comments

--- pages/auth/logout.php
+++ pages/auth/logout.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-session_unset();
-session_destroy();
-header("Location: index.php");
-exit;
+session_start();                // Start a session
+session_unset();                // Make unset the session
+session_destroy();              // Destroy the session
+header("Location: index.php");  // Go back in login form
+exit;                           // Stop
